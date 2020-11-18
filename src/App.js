30312210db4f3daf589221feffb39aea970c82d7
Prashanthp0703 <prashanthp0703@gmail.com>
2020-11-18 10:01:57 +0530

    tinder-front-end

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import SwipeButtons from './SwipeButtons';
+import TinderCards from './TinderCards';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header/>
+        <TinderCards/>
+        <SwipeButtons/>
       </header>
     </div>
   );
